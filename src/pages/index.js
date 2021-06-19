38a15477391184aa4f4a72ede921687ed0a1fe13
Divya Garg <divyagarg2601@gmail.com>
2021-06-19 14:08:46 +0530

    Add tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div class="m-24 text-green-900">Hello world!</div>
+    </Layout>
+  )
 }
