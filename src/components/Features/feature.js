f0559da5cff55f4dd509b47da8dd9e37fa456ccb
Shailesh Aanand <anaandshailu@gmail.com>
2021-06-22 18:08:36 +0530

    Fix feature card rounded and text height

--- src/components/Features/feature.js
+++ src/components/Features/feature.js
@@ -2,11 +2,11 @@ import React from "react"
 
 export default function Feature({ imgSrc, text }) {
   return (
-    <div className="flex-1 rounded-md">
+    <div className="flex-1 rounded-md overflow-hidden">
       <div className="p-8 bg-[#35406E]">
         <img src={imgSrc} alt="Image" />
       </div>
-      <div className="py-5 font-bold px-10 bg-[#1C2653] text-white text-center">
+      <div className="py-5 font-bold px-10 bg-[#1C2653] text-white text-center h-28 self-center flex items-center">
         {text}
       </div>
     </div>
