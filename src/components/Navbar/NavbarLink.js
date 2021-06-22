5850b6d9fa428cbfabc6222a85a014d456557e67
Shailesh Aanand <anaandshailu@gmail.com>
2021-06-22 18:08:15 +0530

    Add hover animated underline

--- src/components/Navbar/NavbarLink.js
+++ src/components/Navbar/NavbarLink.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function NavbarLink({ text, link }) {
   return (
     <div>
-      <a className="font-semibold" href={link}>
+      <a className="font-bold block border-transparent px-0.5 hover:border-gray-900 border-b-2 transition-all duration-300" href={link}>
         {text}
       </a>
     </div>
