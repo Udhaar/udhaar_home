9869b2e4914328adb7b4aec95cc79d7f8d255e59
Shailesh Aanand <anaandshailu@gmail.com>
2021-06-23 12:54:16 +0530

    format with prettier and introduce workspace vscode settings

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,36 @@
-import React, { useState} from "react"
+import React, { useState } from "react"
 import NavbarLink from "./NavbarLink"
 import NavButton from "./NavButton"
 
 export default function Navbar() {
-  const [navbarVisible, setNavbarVisible] = useState(false);
+  const [navbarVisible, setNavbarVisible] = useState(false)
   return (
     <div className="flex flex-col pt-3 md:py-3 md:flex-row justify-between px-3 mb-3 md:mb-0 bg-[#afd6fe]">
       <div className="flex md:w-max w-full justify-between">
         <a href="/">
           <img src="/Logoo.svg" alt="" className="h-10 md:h-14" />
         </a>
-        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => { setNavbarVisible(!navbarVisible)}} className="h-7 w-7 md:hidden self-center " viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clipRule="evenodd" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          onClick={() => {
+            setNavbarVisible(!navbarVisible)
+          }}
+          className="h-7 w-7 md:hidden self-center "
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
         </svg>
       </div>
-      <div className={`flex flex-col md:flex-row items-center gap-5 md:mt-0 mt-3 transition-all ease-in-out duration-500 ${!navbarVisible? `max-h-0 md:max-h-52`:`max-h-52`} overflow-hidden`}>
+      <div
+        className={`flex flex-col md:flex-row items-center gap-5 md:mt-0 mt-3 transition-all ease-in-out duration-500 ${
+          !navbarVisible ? `max-h-0 md:max-h-52` : `max-h-52`
+        } overflow-hidden`}
+      >
         <NavbarLink link="/" text="How it works" />
         <NavbarLink link="/" text="Features" />
         <NavbarLink link="/" text="Contact Us" />
--- src/components/Navbar/NavbarLink.js
+++ src/components/Navbar/NavbarLink.js
@@ -3,7 +3,10 @@ import React from "react"
 export default function NavbarLink({ text, link }) {
   return (
     <div>
-      <a className="font-bold block border-transparent px-0.5 hover:border-gray-900 border-b-2 transition-all duration-300" href={link}>
+      <a
+        className="font-bold block border-transparent px-0.5 hover:border-gray-900 border-b-2 transition-all duration-300"
+        href={link}
+      >
         {text}
       </a>
     </div>
